5f2339316b5a6fdf27e2f4f6a0e9c5d11b6f3476
Jon linaje <jonlinaje00@gmail.com>
2024-10-08 10:43:36 +0200

    Jon linaje 08-10-2024

--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -2,11 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.XR.Interaction.Toolkit.Filtering;
 
 public class ChangeScene : MonoBehaviour
 {
+    public Animator puertaKotxea;
+
     private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.name == "Poke Point")
+        {
+            puertaKotxea.SetTrigger("puertaKotxea");
+        }
+    } 
+    
+    public void cambiarEscena()
     {
         SceneManager.LoadScene("Etxea(Egunez)");
     }
+    
 }
