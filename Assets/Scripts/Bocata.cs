5d466313647c7a139c6fefe8879c29681a35f5ec
Alain <3d23-alainrm@harrobia.net>
2024-10-28 08:21:12 +0100

    Alain 28/10

---
+++ Assets/Scripts/Bocata.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class Bocata : MonoBehaviour
+{
+    public GameObject breadtop;
+    public GameObject breadbottom;
+    public GameObject cheese;
+    public GameObject tomato;
+    public GameObject letuce;
+    public GameObject bocatafinal;
+    public AudioSource po;
+    public GameObject end;
+
+    private void Awake()
+    {
+        breadtop.SetActive(false); cheese.SetActive(false); letuce.SetActive(false); tomato.SetActive(false);
+    }
+    public void BreadBot()
+    {
+        breadbottom.GetComponent<MeshRenderer>().enabled = false; letuce.SetActive(true);
+        breadbottom.GetComponent<XRSocketInteractor>().GetOldestInteractableSelected().transform.gameObject.GetComponent<Collider>().enabled = false;
+        po.Play();
+    }
+    public void Letuce()
+    {
+        letuce.GetComponent<MeshRenderer>().enabled = false; tomato.SetActive(true);
+        letuce.GetComponent<XRSocketInteractor>().GetOldestInteractableSelected().transform.gameObject.GetComponent<Collider>().enabled = false; po.Play();
+    }
+    public void Tomato()
+    {
+        tomato.GetComponent<MeshRenderer>().enabled = false; cheese.SetActive(true);
+        tomato.GetComponent<XRSocketInteractor>().GetOldestInteractableSelected().transform.gameObject.GetComponent<Collider>().enabled = false; po.Play();
+    }
+    public void Cheese()
+    {
+        cheese.GetComponent<MeshRenderer>().enabled = false; breadtop.SetActive(true);
+        cheese.GetComponent<XRSocketInteractor>().GetOldestInteractableSelected().transform.gameObject.GetComponent<Collider>().enabled = false; po.Play();
+    }
+    public void BreadTop()
+    {
+        breadtop.GetComponent<MeshRenderer>().enabled = false;
+        breadtop.GetComponent<XRSocketInteractor>().GetOldestInteractableSelected().transform.gameObject.GetComponent<Collider>().enabled = false; po.Play();
+        Instantiate(bocatafinal, gameObject.transform.position, bocatafinal.transform.rotation);
+        GameObject g = GameObject.Instantiate(end);
+        Destroy(gameObject, 0.08f);
+    }
+}
