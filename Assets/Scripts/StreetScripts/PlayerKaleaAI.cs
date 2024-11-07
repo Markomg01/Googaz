159dfbb270a7e6bbe100b3de6b6c7fd1cc2e2c75
Jon linaje <jonlinaje00@gmail.com>
2024-11-07 08:54:47 +0100

    Jon Linaje 07-11-2024

---
+++ Assets/Scripts/StreetScripts/PlayerKaleaAI.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerKaleaAI : MonoBehaviour
+{
+    public float distanciaRaycast = 100f;
+    public LayerMask kaleaAI;
+    private kaleaAI iaUltimaGolpeada;
+
+    public Renderer kaleaAIrenderer;
+
+    void Update()
+    {
+        Debug.Log("Visible " + kaleaAIrenderer.isVisible);
+        LanzarRaycast();
+    }
+
+    void LanzarRaycast()
+    {
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, distanciaRaycast, kaleaAI))
+        {
+            Debug.Log("GGGGGGG");
+            kaleaAI ia = hit.collider.GetComponent<kaleaAI>();
+
+            if (ia != null)
+            {
+                ia.DetenerIA(true);
+                iaUltimaGolpeada = ia;
+                Debug.Log("Raycast impactó a la IA: " + hit.collider.name);
+            }
+        }
+
+        if( !kaleaAIrenderer.isVisible)
+        {
+            kaleaAI[] ias = FindObjectsOfType<kaleaAI>();
+            foreach (kaleaAI ia in ias)
+            {
+                ia.DetenerIA(false);
+            }
+        }
+
+        Debug.DrawRay(transform.position, transform.forward * distanciaRaycast, Color.red);
+    }
+}
