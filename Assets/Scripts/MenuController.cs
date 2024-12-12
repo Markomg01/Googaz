dfecf2d5c1311bb998a1639c5e9e963251b1867c
Lander <3d23-landerrv@harrobia.net>
2024-12-12 12:48:31 +0100

    Commit12/12

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -7,9 +7,26 @@ public class MenuController : MonoBehaviour
 {
     public string eszena;
 
+    public GameObject sceneButtons;
+    public bool settings =  false;
+
     public void LoadScene(string eszena)
     {
         SceneManager.LoadScene(eszena);
+    }
+
+    public void SceneButtons()
+    {
+        if(!settings)
+        {
+            settings = true;
+            sceneButtons.SetActive(true);
+        }
+        else
+        {
+            settings = false;
+            sceneButtons.SetActive(false);
+        }
     }
 
     public void Exit()
