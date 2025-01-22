51f992eed8489d47eb850e048c6eef1880e7cf50
Lander <3d23-landerrv@harrobia.net>
2025-01-22 13:20:25 +0100

    Timeout

---
+++ Assets/Scripts/TimeOut.cs
@@ -0,0 +1,26 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TimeOut : MonoBehaviour
+{
+    public MachistaBotScript machistaBot;
+
+    private void Update()
+    {
+        if(machistaBot.time == 0)
+        {
+            StartCoroutine(RestartScene());
+        }
+    }
+
+    IEnumerator RestartScene()
+    {
+        gameObject.transform.DOScale(1, .5f);
+        yield return new WaitForSeconds(10f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        yield return null;
+    }
+}
